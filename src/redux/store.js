4fc6865f3df84f5b908c83b9092703a1c91f7134
brayanobisto <brayan-28-@hotmail.com>
2022-07-10 11:27:24 -0500

    Add redux devtools

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,5 +2,6 @@ import { configureStore } from '@reduxjs/toolkit'
 import { globalSlice } from './slices/global'
 
 export const store = configureStore({
+  devTools: false,
   reducer: globalSlice.reducer,
 })
